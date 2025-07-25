346bf99601e2bce34aff2a926599f796432a5b91
Abraham Gebeyehu <118066569+Ab-xo@users.noreply.github.com>
2025-07-25 19:43:02 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ 
-    tailwindcss(),
-    react()],
-    server: {
-      historyApiFallback: true,
-    },
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // ✅ Needed by Render
+  },
+  server: {
+    // This is used for local development only
+    port: 5173,
+  },
 })
